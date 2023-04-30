b96e7bfa653126d0f587b0545019fd6d08db2320
oscrowd <oscrowd@gmail.com>
2023-04-30 13:51:53 +0300

    Добавьте файлы проекта.

---
+++ Module12_BlockShema/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12_BlockShema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var phoneBook = new List<User>();
+            string inputName = null;
+            bool alreadyExists = false;
+
+            // добавляем контакты
+            phoneBook.Add(new User("Andr","Игорь", true));
+            phoneBook.Add(new User("Igor" ,"Андрей", false));
+            phoneBook.Add(new User("Ivan", "Иван", false));
+
+            Console.WriteLine("Введите свое имя:");
+            inputName = Console.ReadLine();
+            Console.WriteLine("Добро пожаловать {0}", inputName);
+            // пробегаемся по списку и смотрим, есть ли уже с таким именем
+            foreach (var contact in phoneBook)
+            {
+                if (contact.Name == inputName)
+                {
+                    alreadyExists = true;
+                    break;
+                }
+            }
+            if (!alreadyExists) 
+            {
+                User.ShowAds();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module12_BlockShema/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Module12_BlockShema
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool ispremium) 
+        { 
+            Login = login; 
+            Name = name;
+            IsPremium = ispremium;
+        }
+
+        public static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
